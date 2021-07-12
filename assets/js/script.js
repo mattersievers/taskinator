00dab884fc53041dfe570d0195e627f53dbf52e2
Matthew Sievers <mattersievers@gmail.com>
2021-07-12 17:33:17 -0500

    buttons function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 var createTasksHandler = function () {
 
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // Create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // Create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML= "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>"+ taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //give it a class name
+    taskInfoEl.className="task-info";
+    
+    //add eniter list item to list
+  
+   
     tasksToDoEl.appendChild(listItemEl);  
-    console.log(event);  
+   
 }
 
 formEl.addEventListener("submit", createTasksHandler);
